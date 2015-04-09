ba66c1fb39d325d9557a4adbe93341b9c4be4d4a
Federico Castaneda <fcastaneda@sciencenews.org>
2015-04-09 15:54:46 -0400

    update

--- js/curiosity.js
+++ js/curiosity.js
@@ -19,13 +19,10 @@ $(document).ready(function(){
 $(window).scroll(function (event) {
   var scroll = $(window).scrollTop();
     if (scroll > 1000) {
-        //document.getElementById("nav-wrapper").style.position = "fixed";
-        //document.getElementById("nav-wrapper").style.top = "40px";
         $(".nav-wrapper").css({"position": "fixed", "top": "40px"}).addClass("nav-wrapper-moving");
     } else {
-        //document.getElementById("nav-wrapper").style.position = "absolute";
-        //document.getElementById("nav-wrapper").style.top = "1040px";
-        $(".nav-wrapper").css({"position": "absolute", "top": "1040px"}).removeClass("nav-wrapper-moving");
+        //$(".nav-wrapper").css({"position": "absolute", "top": "1040px"}).removeClass("nav-wrapper-moving");
+        $(".nav-wrapper").removeClass("nav-wrapper-moving");
     }
 });
 
