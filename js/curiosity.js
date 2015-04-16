2145022c1487941964b84973ebf4e066d81c0b1a
Federico Castaneda <fcastaneda@sciencenews.org>
2015-04-16 14:51:21 -0400

    update

--- js/curiosity.js
+++ js/curiosity.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
 
-  $(window).scroll(function (event) {
-    var scroll = $(window).scrollTop();
-      if (scroll > 1000) {
-        $(".nav-wrapper").css({"position": "fixed", "top": "40px"}).addClass("nav-wrapper-moving");
-      } else {
-        $(".nav-wrapper").css({"position": "absolute", "top": "1040px"}).removeClass("nav-wrapper-moving");
-      }
-  });
+  // $(window).scroll(function (event) {
+  //   var scroll = $(window).scrollTop();
+  //     if (scroll > 1000) {
+  //       $(".nav-wrapper").css({"position": "fixed", "top": "40px"}).addClass("nav-wrapper-moving");
+  //     } else {
+  //       $(".nav-wrapper").css({"position": "absolute", "top": "1040px"}).removeClass("nav-wrapper-moving");
+  //     }
+  // });
+
+// var header = $("#fullpage");
+//   $(window).scroll(function() {    
+//     var scroll = $(window).scrollTop();
+//        if (scroll >= window.innerHeight) {
+//           header.addClass("mars-nav-wrapper-moving");
+//         } else {
+//           header.removeClass("mars-nav-wrapper-moving");
+//         }
+// });
 
   $("#fullpage").fullpage({
     //menu: '#curiosity-nav',
